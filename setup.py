6e737929da95b2d6c1043beae65b338619b425fa
Peter Laudel <peter.laudel@gmail.com>
2020-01-02 20:28:21 +0100

    Add long description

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ with open('README.md') as f:
 
 setup(
     name="json-api-smart",
-    version="0.0.4b",
+    version="0.0.3b",
     packages=["src"],
     url="https://github.com/NilssonPL/json-api-smart",
     license="MIT",
     author="Nilsson",
     author_email="peter.laudel@gmail.com",
-    description=long_description,
-    long_description="file: README.md",
+    description="Is your client library which support ths JSON:API standard.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # This is important!
     install_requires=["requests", "inflect", "typeguard"],
     extras_require={"dev": ["pytest", "requests_mock", "coverage", "mypy"]},
     keywords="json:api api json rest resources",
